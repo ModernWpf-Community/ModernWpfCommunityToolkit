965a69072e3c0cd8a0c3cc6ad8917b8eff54c953
Shankar <63704247+ShankarBUS@users.noreply.github.com>
2020-07-01 12:55:03 +0530

    [Feature] Eyedropper & EyedropperToolButton. (#1)
    * Eyedropper - Initial commit
    
    * Basic working implementation
    
    * Complete Implementation of Eyedropper :)

--- ModernWpf.Toolkit.SampleApp/MainWindow.xaml.cs
+++ ModernWpf.Toolkit.SampleApp/MainWindow.xaml.cs
@@ -1,12 +1,23 @@
-﻿using System.Windows;
+﻿using ModernWpf.Toolkit.Controls;
+using System.Windows;
 
 namespace ModernWpf.Toolkit.SampleApp
 {
     public partial class MainWindow : Window
     {
+        Eyedropper globalEyedropper;
+
         public MainWindow()
         {
             InitializeComponent();
+            globalEyedropper = new Eyedropper();
+            globalEyedropper.ColorChanged += (s, e) => { TargetSolidColorBrush.Color = e.NewColor; };
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var color = await globalEyedropper.Open();
+            TargetSolidColorBrush.Color = color;
         }
     }
 }
