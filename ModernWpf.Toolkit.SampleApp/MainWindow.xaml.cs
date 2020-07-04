b36bed2c6c3ee3857fb23dc4df294f7f3e6f1f43
Shankar <shankarbus04082001@hotmail.com>
2020-07-04 11:36:03 +0530

    feature : Ported SimpleWrapPanel aka WrapPanel.
    (named it SimpleWrapPanel to avoid confusion with existing one in WPF and to match ModernWpf's SimpleStackPanel naming convention)

--- ModernWpf.Toolkit.SampleApp/MainWindow.xaml.cs
+++ ModernWpf.Toolkit.SampleApp/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using ModernWpf.Toolkit.UI.Controls;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ModernWpf.Toolkit.SampleApp
 {
--- ModernWpf.Toolkit.UI.Controls/TokenizingTextBox/TokenizingTextBox.cs
+++ ModernWpf.Toolkit.UI.Controls/TokenizingTextBox/TokenizingTextBox.cs
@@ -8,7 +8,6 @@ using ModernWpf.Toolkit.UI.Extensions;
 using ModernWpf.Toolkit.UI.Helpers;
 using System;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
